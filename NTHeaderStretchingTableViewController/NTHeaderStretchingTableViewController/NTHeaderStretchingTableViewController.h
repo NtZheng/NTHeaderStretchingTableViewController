369600036f0024d8d486da35bac09ae81302fcec
NtZheng <2745674085@qq.com>
2016-10-10 14:19:02 +0800

    搭建好了大致框架..

---
+++ NTHeaderStretchingTableViewController/NTHeaderStretchingTableViewController/NTHeaderStretchingTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NTHeaderStretchingTableViewController.h
+//  NTHeaderStretchingTableViewController
+//
+//  Created by Nineteen on 10/10/16.
+//  Copyright © 2016 Nineteen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTHeaderStretchingTableViewController : UITableViewController
+
+@end
